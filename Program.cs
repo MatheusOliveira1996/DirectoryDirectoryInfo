4d329df6d6c687a55086fa5c3f517a3a9936fe85
Matheus Oliveira <matheus.hortolandia@gmail.com>
2022-10-01 12:52:45 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryDirectoryInfo {
+    class Program {
+        static void Main (string[] args) {
+            string path = @"D:\Users\Matheus\Desktop\Curso - C# - Udemy\Projetos\myfolder";
+
+            try {
+                var folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders) {
+                    Console.WriteLine(s);
+                }
+
+                var files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files) {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(@"D:\Users\Matheus\Desktop\Curso - C# - Udemy\Projetos\myfolder\newfolder");
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
